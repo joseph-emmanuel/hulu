eeeaab5647b1dcfab0dbd2fe6ec2df66d91eea6f
Joseph Emmanuel <joseph.emmanuel@gbsnbkj001.greatgulf.biz>
2022-01-05 18:17:16 -0500

    completed the full build and ready for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com", "image.tmdb,org"],
+    domains: ["links.papareact.com", "image.tmdb.org"],
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,38 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+//Client render
+export default function Home({ results }) {
   return (
-    <div className="">
-      <head>
-        <title>Hulu</title>
+    <div>
+      <Head>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
-      </head>
-      {/* Header */}
+      </Head>
+
       <Header />
-
-      {/* Navbar */}
       <Nav />
-
-      {/* Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+//Server side render
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
